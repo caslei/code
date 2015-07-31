28d133a937073ecc4876cc268bb67eb6e1dcdb21
Joris Roels <Joris.Roels@telin.ugent.be>
2015-07-31 16:19:52 +0200

    Compilation bug fix in graph based segmentation.

--- functions/labels2edges.m
+++ functions/labels2edges.m
@@ -2,8 +2,10 @@
 % Converts a labeled segmentation to segmentation boundaries
 function edges = labels2edges(labels)
 
-grad = imfilter(labels,[3 -1; -1 -1]);
+grad = imfilter(labels,[-1 3; -1 -1]);
 edges = abs(grad)>0;
+
+% edges = edge(labels,0);
 
 end
 
--- superpixels/graph_based/demo_GB.m
+++ superpixels/graph_based/demo_GB.m
@@ -8,16 +8,16 @@ img = double(imread('lena.tif'));
 
 %% GB parameters
 sigma = 0.5;
-k = 500;
+k = 100;
 min = 20;
 
 %% GB algorithm
 tic
-labels = fGB(img,N,sigma,k,min);
+labels = fGB(img,sigma,k,min);
 toc
 boundary = labels2edges(labels);
 labeled = fill_segments(img,labels);
 
 %% Show superpixel segmentation
-figure,imshow(mask(img,boundary),[0,255]);
-figure,imshow(labeled,[0,255]);
+figure,imshow(uint8(mask(img,boundary)),[0,255]);
+figure,imshow(uint8(labeled),[0,255]);
